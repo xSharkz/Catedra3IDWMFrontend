5b9ae48295e2f8c70b32bd59f5a4f433021c2748
xSharkz <105187265+xSharkz@users.noreply.github.com>
2025-01-08 23:24:50 -0300

    feat: add auth guard features.

--- src/app/Guards/auth.guard.ts
+++ src/app/Guards/auth.guard.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthService } from '../Services/auth.service';
+import { ToastService } from '../Services/toast.service';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router, private toastService: ToastService) {}
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    if (this.authService.isAuthenticated()) {
+    if (this.authService.isAuthenticated() && this.authService.isTokenValid()) {
       return true;
     } else {
+      this.authService.logout();
+      this.toastService.show('Debe iniciar sesión.')
       this.router.navigate(['/login']);
       return false;
     }
