b8fb41c93fb0472732e20537a35fa5bb6b1c5d06
xSharkz <105187265+xSharkz@users.noreply.github.com>
2025-01-08 23:23:41 -0300

    feat: add toastservice to app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,23 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { routes } from './app.routes';
+import { ToastService } from './Services/toast.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [RouterModule, RouterOutlet]
+  imports: [RouterModule, RouterOutlet, CommonModule]
 })
 export class AppComponent {
+  toastMessage: string | null = null;
   title = 'Catedra3IDWMFrontend';
-  constructor() {
+  constructor(private toastService: ToastService) {
     RouterModule.forRoot(routes); 
+    this.toastService.toastMessages$.subscribe(message => {
+      this.toastMessage = message;
+      setTimeout(() => this.toastMessage = null, 3000); // Ocultar el mensaje después de 3 segundos
+    });
   }
 }
